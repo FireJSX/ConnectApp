2ba0a1053f520d568489b4e9eaf272df4a929157
firejsx <firestroke.ghg@gmail.com>
2024-11-18 12:57:29 +0100

    NFC und Layout

---
+++ app/src/main/java/com/example/connectapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.connectapp;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private NfcAdapter nfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "NFC wird auf diesem Gerät nicht unterstützt", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableForegroundDispatch();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableForegroundDispatch();
+    }
+
+    private void enableForegroundDispatch() {
+        Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE);
+        IntentFilter[] filters = new IntentFilter[]{};
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, filters, null);
+    }
+
+    private void disableForegroundDispatch() {
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+}
